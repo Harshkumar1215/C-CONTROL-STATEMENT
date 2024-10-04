ef7c83da3ce0c83c93d81ad427b2e84260d86afa
Harshkumar1215 <harshharsh1215225@gmail.com>
2024-10-04 15:00:11 +0530

    for loop

---
+++ forLoop .cs
@@ -0,0 +1,18 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int i,j;
+       for( i = 0;i< 5; i++)
+       {
+        
+     
+               Console.WriteLine ( " *");
+               
+    
+       }
+
+    }
+}
